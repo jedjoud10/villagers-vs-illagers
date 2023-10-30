ea063b3f8b46b7856de5be89e200edf7474d8c87
Jedjoud10 <jribijed6@gmail.com>
2023-10-30 13:10:19 -0400

    ambatapush

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,21 @@
 #[cfg(feature = "buddy-alloc")]
 mod alloc;
 mod wasm4;
+mod sprites;
+pub use sprites::*;
 use wasm4::*;
 
 static mut GAME: Option<Game> = None;
+
+// Potential state of each cell
+#[derive(Clone, Copy)]
+enum CellState {
+    Empty,
+    // add villager states
+    // add illager states
+    // add house state   
+}
+
 
 // p1: villager
 // p2: illager
@@ -13,36 +25,37 @@ struct Game {
     cursors: [u8; 2],
     old_gamepad: [u8; 2],
     new_gamepad: [u8; 2],
-    grid: [u8; 24],
+    current_selected_class: [u8; 2],
+    grid: [CellState; 192],
 }
 
 impl Game {
     unsafe fn new() -> Self {
-        *PALETTE = [
-            0xd7bbad, 0xbf8a6e, 0x57795b, 0x26180e
-        ];
+        // grey, beige, green, brown
+        *PALETTE = [0xd7bbad, 0xbf8a6e, 0x57795b, 0x26180e];
 
         Self {
-            villager: 5, 
+            villager: 5,
             illager: 5,
             cursors: [0; 2],
             new_gamepad: [0; 2],
             old_gamepad: [*GAMEPAD1, *GAMEPAD2],
-            grid: [0; 24],
+            current_selected_class: [0, 0],
+            grid: [CellState::Empty; 192],
         }
     }
-    
+
     unsafe fn run(&mut self) {
         self.fetch_input();
-        self.move_cursor();
-        self.draw_background_grid();
+        self.draw_background();
+        self.draw_sprites();
         self.draw_footer();
         self.draw_cursors();
-        
-        //debug_palette();
+        self.debug_palette();
     }
 
     // Fetch gamepad input. Also works in multiplayer. Only supports 2 players
+    // Also moves the appropriate selectors if necessary
     unsafe fn fetch_input(&mut self) {
         const GAMEPADS: [*const u8; 2] = [GAMEPAD1, GAMEPAD2];
         for index in 0..2 {
@@ -51,26 +64,29 @@ impl Game {
             let new = current & (last ^ current);
             self.old_gamepad[index] = current;
             self.new_gamepad[index] = new;
-        }
-    }
 
-    // Move the cursor based on the input
-    unsafe fn move_cursor(&mut self) {
-        for index in 0..2 {
+            // Move cursor on grid
             let x = self.new_gamepad[index];
-            
-            // TODO: Refactor
-            if x & BUTTON_UP != 0 {
-                self.cursors[index] = self.cursors[index].saturating_sub(16);
+            let pos = self.cursors[index];
+            self.cursors[index] = if x & BUTTON_UP != 0 {
+                pos.saturating_sub(16)
             } else if x & BUTTON_DOWN != 0 {
-                self.cursors[index] = self.cursors[index].saturating_add(16);
+                pos.saturating_add(16)
             } else if x & BUTTON_LEFT != 0 {
-                self.cursors[index] = self.cursors[index].saturating_sub(1);
+                pos.saturating_sub(1)
             } else if x & BUTTON_RIGHT != 0 {
-                self.cursors[index] = self.cursors[index].saturating_add(1);
-            }
+                pos.saturating_add(1)
+            } else {
+                pos
+            }   
+            .clamp(0, 191);
 
-            self.cursors[index] = self.cursors[index].clamp(0, 191);
+            // Cycle current selected class
+            if new & BUTTON_2 != 0 {
+                let selected = &mut self.current_selected_class[index];
+                *selected += 1;
+                *selected = *selected % 3;
+            }
         }
     }
 
@@ -78,36 +94,60 @@ impl Game {
     unsafe fn draw_footer(&mut self) {
         *DRAW_COLORS = 0b1000000;
         rect(0, 120, 160, 40);
-    
+
         *DRAW_COLORS = 0b0100_0000_0000_0100;
         text("V: ", 2, 122);
         text("I: ", 2, 132);
         text([self.villager + 48], 16, 122);
         text([self.illager + 48], 16, 132);
+
+        *DRAW_COLORS = 0b0100_0011_0010_0001;
+
+        // Draw mini-icon for selectors
+        for index in 0..2i32 {
+            /*
+            match index {
+                0 => {
+                    //blit(&SPRITE, 120, 122, SPRITE_WIDTH, SPRITE_HEIGHT, SPRITE_FLAGS);
+
+                },
+                1 => {
+
+                }
+                _ => {}
+            }
+            */
+        }
     }
 
-    // Draw the background tiles for the grid
-    unsafe fn draw_background_grid(&self) {
-        for x in 0..16i32 {
-            for y in 0..12i32 {
-                *DRAW_COLORS = if ((x % 2) == 0) ^ ((y % 2) == 0) { 2 } else { 3 };
-                rect(x * 10, y * 10, 10, 10)
+    // Draw the background color
+    unsafe fn draw_background(&self) {
+        *DRAW_COLORS = 0b0011_0011_0011_0011;
+        rect(0, 0, 160, 120);
+    }
+
+    // Draw the grid with the appropriate sprites
+    unsafe fn draw_sprites(&self) {
+        for (index, state) in self.grid.iter().enumerate() {
+            // TODO: Draw appropriate sprite here
+            match state {
+                CellState::Empty => {},
             }
         }
     }
 
     // Draw the player cursors. Different colors assigned to each team
     unsafe fn draw_cursors(&self) {
-        for (index, pos) in self.cursors.iter().enumerate() {
-            let posy = pos / 16;
-            let posx = pos % 16;
+        for (index, selector_position) in self.cursors.iter().enumerate() {
+            let posy = selector_position / 16;
+            let posx = selector_position % 16;
 
             const COLORS: [u8; 2] = [0b1000000, 0b0010000];
             *DRAW_COLORS = COLORS[index] as u16;
-            rect((posx * 10) as i32,(posy * 10) as i32, 10, 10);
+            rect((posx * 10) as i32, (posy * 10) as i32, 10, 10);
         }
     }
-    
+
     // Draw a debug palette at the bottom right corner
     unsafe fn debug_palette(&self) {
         *DRAW_COLORS = 0b0100_0000_0000_0001;
@@ -120,7 +160,6 @@ impl Game {
         rect(150, 150, 10, 10);
     }
 }
-
 
 #[no_mangle]
 unsafe fn start() {
